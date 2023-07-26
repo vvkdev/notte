75e9428e0ada7e76ddad1f278173bf0d1647105e
vvkdev <vvkdeveloper@gmail.com>
2023-07-26 17:31:26 +0300

    add edit fragment

---
+++ app/src/main/java/com/vvkdev/notte/EditFragment.kt
@@ -0,0 +1,34 @@
+package com.vvkdev.notte
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.vvkdev.notte.databinding.FragmentEditBinding
+
+class EditFragment : Fragment() {
+
+    private var _binding: FragmentEditBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentEditBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fabSave.setOnClickListener { findNavController().popBackStack() }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

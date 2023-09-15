96356b2a4bef533e106929efb1767d89d382eedd
vvkdev <vvkdeveloper@gmail.com>
2023-09-16 02:28:43 +0300

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,11 +33,11 @@ android {
 dependencies {
     implementation(project(mapOf("path" to ":core:ui")))
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
